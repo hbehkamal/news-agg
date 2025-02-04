f319987b0f2823f9432c1062a2ca45c6da920401
Hosein Behkamal <hoseynbehkamal@gmail.com>
2025-02-04 20:20:47 +0330

    chore: add basic API codes

---
+++ src/api/index.ts
@@ -0,0 +1,14 @@
+import { Post } from "@/types";
+import axios from "axios";
+
+export const getNews = async (): Promise<Post[]> => {
+  try {
+    // TODO: add parameters
+    const response = await axios.get("/api/get-aggregated-news");
+
+    return response.data as Post[];
+  } catch (error) {
+    console.error("Error:", error);
+    return [];
+  }
+};
